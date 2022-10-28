8282c4c13afcc2673789b2bdb1e815871d6b7801
yergalem1 <yergalemt@gmail.com>
2022-10-27 18:35:33 -0700

    my First commit

---
+++ eliab_movie/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const App =() =>{
+ // const [state, setState] = useState([intitalState]);
+  const [films, setFilms] = useState([]);
+           
+  
+  return (
+    <div className="App">
+
+     
+    
+    </div>
+  );
+}
+}
+
+
+export default App;
